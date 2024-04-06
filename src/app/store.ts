2318de5364577a96ed7d008dbf102b89a94589e0
Yevhenii <zheki2004@gmail.com>
2024-04-06 16:09:11 +0300

    feat: added persistent provider

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { repositoryReducer } from "./slices/repositorySlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
 export const store = configureStore({
   reducer: {
     repository: repositoryReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import { store } from "./app/store.ts";
+import { persistor, store } from "./app/store.ts";
 import "./index.scss";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
