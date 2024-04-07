57a4b7535ba1af84d109812c8df6d1922f59444f
Yevhenii <zheki2004@gmail.com>
2024-04-07 13:52:29 +0300

    fix: remove secret key

--- src/api/core.ts
+++ src/api/core.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = import.meta.env.VITE_SERVER_URL || 'https://api.github.com';
 
-export const token = import.meta.env.VITE_ACCESS_TOKEN || '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';
+export const token = import.meta.env.VITE_ACCESS_TOKEN;
